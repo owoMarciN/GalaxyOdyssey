e65432ee5dc25c2670345d1bc6dd01a56a370713
Silly_Gamer <basisty.marcin@gmail.com>
2024-06-15 15:12:14 +0200

    Add files via upload

---
+++ src/DeathScreen.cpp
@@ -0,0 +1,83 @@
+#include "DeathScreen.h"
+
+
+DeathScreen::DeathScreen(){
+
+    mInput = InputManager::Instance();
+
+    mDeathSurface = new GameEntity(Vector2(Graphics::Instance()->SCREEN_WIDTH*0.5f, Graphics::Instance()->SCREEN_HEIGHT*0.5f));
+    mBackground = new Texture("DeathScreen.png");
+    mDeath = new Texture("MISSION FAILED!" , "Arcade_Classic.ttf", 96, {205, 0, 0});
+    mRestart = new Texture("RESTART", "Karmatic_Arcade.ttf", 32, {200, 200, 200});
+    mMenu = new Texture("MENU", "Karmatic_Arcade.ttf", 32,{200, 200, 200});
+    mCursor = new Texture("--", "Karmatic_Arcade.ttf", 32, {250, 250, 250});
+
+    mBackground->Parent(mDeathSurface);
+    mDeath->Parent(mDeathSurface);
+    mRestart->Parent(mDeathSurface);
+    mMenu->Parent(mDeathSurface);
+    mCursor->Parent(mDeathSurface);
+
+    mBackground->Pos(VEC2_ZERO);
+    mDeath->Pos(Vector2(0.0f, -100.0f));
+    mRestart->Pos(Vector2(-10.0f, 20.0f));
+    mMenu->Pos(Vector2(-10.0f, 95.0f));
+    mCursor->Pos(Vector2(-150.0f, 20.0f));
+    mCursorStart = mCursor->Pos(local);
+    mCursorOffset = Vector2(0.0f, 72.0f);
+    SelectMode = 0;
+
+    mDeathSurface->Parent(this);
+
+}
+
+DeathScreen::~DeathScreen(){
+    mInput = nullptr;
+    
+    delete mBackground;
+    mBackground = nullptr;
+
+    delete mRestart;
+    mRestart = nullptr;
+
+    delete mMenu;
+    mMenu = nullptr;
+
+    delete mCursor;
+    mCursor = nullptr;
+
+    delete mDeath;
+    mDeath = nullptr;
+
+    delete mDeathSurface;
+    mDeath = nullptr;
+
+}
+
+void DeathScreen::ChangeMode(int mode){
+    SelectMode += mode;
+    if(SelectMode < 0)
+        SelectMode = 1;
+    else if(SelectMode > 1)
+        SelectMode = 0;
+    mCursor->Pos(mCursorStart + mCursorOffset * SelectMode);
+}
+
+void DeathScreen::Update(){
+    if(mInput->KeyPressed(SDL_SCANCODE_S))
+        ChangeMode(1);
+    else if(mInput->KeyPressed(SDL_SCANCODE_W))
+        ChangeMode(-1);
+}
+
+int DeathScreen::SelectedMode(){
+    return SelectMode;
+}
+
+void DeathScreen::Render(){
+    mBackground->Render();
+    mDeath->Render();
+    mRestart->Render();
+    mMenu->Render();
+    mCursor->Render();
+}
---
+++ src/Enemy.cpp
@@ -0,0 +1,109 @@
+#include "Enemy.h"
+
+const int Enemy::MAX_BULLETS = 3;
+
+Enemy::Enemy(int path, ENEMYTYPES mEnemy){
+    switch(mEnemy){
+        case ALPHA:
+            mType = ALPHA;
+            mHP = 30;
+            mScore = 300;
+            mSpeed = 100.0f;
+
+            for(int i = 0; i < MAX_BULLETS; i++)
+                mBullet.push_back(new Bullet(1, Bullet::E_NORMAL));
+
+            mEnemyTex = new Texture("EnemyType1.png");
+            mEnemyTex->Scale(VEC2_ONE*0.25f);
+            break;
+        case BETA:
+            mType = BETA;
+            mHP = 30;
+            mScore = 500;
+            mSpeed = 115.0f;
+
+            for(int i = 0; i < MAX_BULLETS; i++)
+                mBullet.push_back(new Bullet(1, Bullet::E_BOUNCING));
+            
+            mEnemyTex = new Texture("EnemyType2.png");
+            mEnemyTex->Scale(VEC2_ONE*0.25f);
+            break;
+        case GAMMA:
+            mType = GAMMA;
+            mHP = 70;
+            mScore = 700;
+            mSpeed = 120.0f;
+
+            for(int i = 0; i < MAX_BULLETS; i++)
+                mBullet.push_back(new Bullet(1, Bullet::E_TRACKING));
+            
+            mEnemyTex = new Texture("EnemyType3.png");
+            mEnemyTex->Scale(VEC2_ONE*0.06f);
+            break;
+    }
+
+    mCurrPath = path;
+    mCurrWayPoint = 0;
+    Pos(sPaths[mCurrPath][mCurrWayPoint]);
+
+    mEnemyTex->Parent(this);
+    mEnemyTex->Pos(VEC2_ZERO);
+
+    mDestruction = new AnimatedTexture("Explosion_V2.png", 90.0f, 0.0f, 100.0f, 95.0f, 46, 0.6f, AnimatedTexture::horizontal);
+    mDestruction->WrapMode(AnimatedTexture::once);
+    mDestruction->Pos(Pos());
+    mDestruction->Parent(this);
+}
+
+Enemy::~Enemy(){
+
+    for(auto &bullet : mBullet){
+        delete bullet;
+        bullet = nullptr;
+    }
+
+    delete mEnemyTex;
+    mEnemyTex = nullptr;
+
+    delete mDestruction;
+    mDestruction = nullptr;
+}
+
+void Enemy::HandleFiring(){
+    if(Active() && InPosition() && NoCollisions()){
+        Uint32 currentTime = SDL_GetTicks();
+        if(currentTime - mLastFiredTime >= mFireDelay){
+            for(int i = 0; i < MAX_BULLETS; i++){
+                if(!mBullet[i]->Active()){
+                    Vector2 pos = Vector2(Pos().x, Pos().y + 30.0f);
+                    mBullet[i]->FireBullet(pos);
+                    mLastFiredTime = currentTime;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void Enemy::Update(){
+    if(Active()){
+        HandleStates();
+        HandleFiring();
+    }
+    else
+        mDestruction->Update();
+
+    for(auto &bullet : mBullet)
+        bullet->Update(); 
+}
+
+void Enemy::Render(){
+    for(auto &bullet : mBullet)
+        bullet->Render();
+
+    if(Active())
+        mEnemyTex->Render();
+    else{
+        mDestruction->Render();
+    }
+}
